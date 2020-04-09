a360bed074835f165640531de2254930abf96033
Kerry Smith <kerrysfs@gmail.com>
2020-04-08 23:19:54 -0700

    fixedfivedayuv

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,17 @@ $(document).ready(function () {
     const $stateBox = $('.stateBox');
     const $submitBtn = $('.submitBtn');
     const apiKey = 'cb729ab815f55696fb5b98b5e8f340f6';
-    
+
+    let timeDate = moment().format("LLLL")
+    // $("#currentDay").text(timeDate);
+
+
+
     $submitBtn.on('click', function () {
         let city = $cityBox.val().trim();
         let state = $stateBox.val().trim();
         dailyWeather(city, state);
-        
+
     })
 
     function dailyWeather(city, state) {
@@ -21,34 +26,56 @@ $(document).ready(function () {
             method: "GET"
         })
             .then(function (response) {
-                console.log(response);
-
+                // console.log(response);
+                // console.log(queryUrl);
                 let name = response.name;
                 let temp = response.main.temp;
                 let icon = response.weather[0].icon;
                 let windSpeed = response.wind.speed;
                 let humidity = response.main.humidity;
+                let lat = response.coord.lat;
+                let lon = response.coord.lon;
 
-
-                let $name = $('<p>').text(name).attr('class', 'name');
+                let $name = $('<h3>').text(name).attr('class', 'name');
                 let $temp = $('<p>').text(temp).attr('class', 'temp');
                 let $icon = $('<img>').attr('src', `http://openweathermap.org/img/w/${icon}.png`);
                 let $windSpeed = $('<p>').text(windSpeed).attr('class', 'wind');
                 let $humidity = $('<p>').text(humidity).attr('class', 'humidity');
-
+                
                 $daily.append($name);
-                $daily.append($temp);
                 $daily.append($icon);
+                $daily.append($temp);
                 $daily.append($windSpeed);
                 $daily.append($humidity);
+                
                 forcastFive(city, state);
-
-
+                uvIndex(lat,lon);
             })
     }
 
-    function forcastFive(city, state) {
+    function uvIndex(lat,lon) {
+        const uvUrl = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
 
+        $.ajax({
+            url: uvUrl,
+            method: "GET"
+        })
+            .then(function (response){
+               let uvIndex = response.value;
+               let $uvIndex = $('<p>').text(uvIndex).attr('class', 'uvIdx');
+            
+                $daily.append($uvIndex);
+                
+                dailyWeather();
+                
+                //forcastFive(city, state);
+            })
+
+
+
+    }
+
+    function forcastFive(city, state) {
         const fiveUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city},${state}&appid=${apiKey}`;
 
         $.ajax({
@@ -56,42 +83,47 @@ $(document).ready(function () {
             method: "GET"
         })
             .then(function (response) {
-                // console.log(response);
-                // console.log(fiveUrl);
-                // console.log(response.list.length);
-                
-                var list = response.list; 
-                // console.log(list[10].main.temp);
-                // for(let i = 0; i < list.length; i++){
-                for (let i = 0; i <list.length; i++){
-                //     let fiveCast = list[i].main;
-                //    let temp = list[i].main.temp;
-                //    let humidity = list[i].main.humidity;
-                // //    let date = list[i].
-                   
-                   
-                   
-                    // console.log(list[i].main.humidity);
-                    // console.log(fiveCast)
+
+                var list = response.list;
+                for (let i = 0; i < list.length; i++) {
+                    let rawDate = list[i].dt_txt;
+                    let date = moment(rawDate).format('H');
+
+                    if (date === '12') {
+                        let temp = list[i].main.temp
+                        // let calcTemp = (temp - 273.15) * 1.80 + 32;
+                        // let tempF = Math.floor(calcTemp);
+                        
+                        // console.log(temp);
+                        let humidity = list[i].main.humidity;
+                        let icon = list[i].weather[0].icon;
+
+                        let $temp = $('<p>').text(temp).attr('class', 'temp');
+                        // let $date = $('<p>').text(date).attr('class', 'date');
+                        let $humidity = $('<p>').text(humidity).attr('class', 'humidity');
+                        let $icon = $('<img>').attr('src', `http://openweathermap.org/img/w/${icon}.png`).attr('class', 'weatherImg');
+
+                        let $divEl = $('<div>').attr('class', 'card');
+                        
+                        $divEl.append($icon);
+                        $divEl.append($temp);
+                        $divEl.append($humidity);
+                        // $divEl.append($date);
+                        
+                        $fiveDay.append($divEl);
+                        
+
+                        
+                        // console.log(tempF);
+                    }
+
+
                 }
             })
-                
-                
+
+
     }
 
 
 
 });
-
-
-
-//c (response.response.docs[i].headline.main)
-// User puts city into input box
-//user clicks on submit button
-//api returns weather for city and provides a 5 day forcast
-//I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-//
-
-
-//loop through array
-//check the DT text value and if it === to what we are looking for then we will append all the data to our page.
